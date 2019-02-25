069190a367847e72063b31361ad8be41098a5d75
casprice <c2price@ucsd.edu>
2019-02-24 17:40:04 -0800

    Add new event page

--- parkinsons_app/lib/main.dart
+++ parkinsons_app/lib/main.dart
@@ -167,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: new Text(widget.title),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: _pushNewEvent,
         child: Icon(Icons.add),
         tooltip: 'Add Event',
       ),
@@ -244,4 +244,107 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  void _pushNewEvent() {
+    final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+    List<String> _colors = <String>['', 'red', 'green', 'blue', 'orange'];
+    String _color = '';
+    // Route to favorites page
+    Navigator.of(context).push(
+      // Pushes the route to the favorites page to the Navigator's stack
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+
+          // Horizontal dividers
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Add Event'),
+            ),
+            body: new SafeArea(
+                top: false,
+                bottom: false,
+                child: new Form(
+                    key: _formKey,
+                    autovalidate: true,
+                    child: new ListView(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.person),
+                            hintText: 'Event',
+                            labelText: 'Enter title',
+                          ),
+                        ),
+                        
+                        new IconButton(
+                            icon: Icon(Icons.calendar_today),
+                            onPressed: (){_selectDate(context);}
+                        ),
+                        new Text('Date selected: ${DateFormat('MMMM dd, yyyy').format(_date)}'),
+                        new TextFormField(
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.calendar_today),
+                            hintText: 'Enter your date of birth',
+                            labelText: 'Dob',
+                          ),
+                          keyboardType: TextInputType.datetime,
+                        ),
+
+                        new TextFormField(
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.email),
+                            hintText: 'Enter a email address',
+                            labelText: 'Email',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new FormField(
+                          builder: (FormFieldState state) {
+                            return InputDecorator(
+                              decoration: InputDecoration(
+                                icon: const Icon(Icons.color_lens),
+                                labelText: 'Color',
+                              ),
+                              isEmpty: _color == '',
+                              child: new DropdownButtonHideUnderline(
+                                child: new DropdownButton(
+                                  value: _color,
+                                  isDense: true,
+                                  onChanged: (String newValue) {
+                                    setState(() {
+                                      //newContact.favoriteColor = newValue;
+                                      _color = newValue;
+                                      state.didChange(newValue);
+                                    });
+                                  },
+                                  items: _colors.map((String value) {
+                                    return new DropdownMenuItem(
+                                      value: value,
+                                      child: new Text(value),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        new Container(
+                            padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+                            child: new RaisedButton(
+                              child: const Text('Submit'),
+                              onPressed: null,
+                            )
+                        ),
+                      ],
+                    )
+                )
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+
 }
